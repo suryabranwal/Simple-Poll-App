c91bf37ebc55d9a909d9493ee15ec3d84c7b8a30
SURAJ BRANWAL <43700439+suryabranwal@users.noreply.github.com>
2020-02-10 23:46:08 +0530

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls', namespace="polls")), #also add app name to polls/url.py
+    path('', include('polls.urls', namespace="polls")), #also add app name to polls/url.py
 ]
